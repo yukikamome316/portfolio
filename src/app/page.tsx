74429fa8dd1f4fdd48e280f5e145bf0e29122fc7
yukikamome316 <32415464+yukikamome316@users.noreply.github.com>
2023-07-04 09:10:45 +0900

    fix: 位置の指定をautoに変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,48 +6,50 @@ import { SocialList } from '@/ui/feature/account/socialList';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.profileContainer}>
-        <Image
-          src="/Cinnamoroll_integrated_512px.jpg"
-          alt="Profile"
-          width={300}
-          height={300}
-          loading="eager"
-          style={{
-            objectFit: 'cover',
-            borderRadius: '50%',
-            userSelect: 'none',
-          }}
-        />
-        <div className={styles.profileContent}>
-          <div className={styles.profileText}>
-            <p className={styles.nameText}>Yuki</p>
-            <p className={styles.usernameText}>yukikamome316</p>
+      <div className={styles.headContainer}>
+        <div className={styles.profileContainer}>
+          <Image
+            src="/Cinnamoroll_integrated_512px.jpg"
+            alt="Profile"
+            width={300}
+            height={300}
+            loading="eager"
+            style={{
+              objectFit: 'cover',
+              borderRadius: '50%',
+              userSelect: 'none',
+            }}
+          />
+          <div className={styles.profileContent}>
+            <div className={styles.profileText}>
+              <p className={styles.nameText}>Yuki</p>
+              <p className={styles.usernameText}>yukikamome316</p>
+            </div>
+            <hr className={styles.line}/>
+            <div className={styles.line}></div>
+            <SocialList />
           </div>
-          <hr className={styles.line}/>
-          <div className={styles.line}></div>
-          <SocialList />
         </div>
-      </div>
 
-      <div className={styles.buttonsList}>
-        <ul className={styles.navList}>
-          <li className={styles.navListItem}>
-            <Link href="/about" className={styles.navLink} passHref>
-              About
-            </Link>
-          </li>
-          <li className={styles.navListItem}>
-            <Link href="/blog" className={styles.navLink} passHref>
-              Blog
-            </Link>
-          </li>
-          <li className={styles.navListItem}>
-            <Link href="/contact" className={styles.navLink} passHref>
-              Contact
-            </Link>
-          </li>
-        </ul>
+        <div className={styles.buttonsList}>
+          <ul className={styles.navList}>
+            <li className={styles.navListItem}>
+              <Link href="/about" className={styles.navLink} passHref>
+                About
+              </Link>
+            </li>
+            <li className={styles.navListItem}>
+              <Link href="/blog" className={styles.navLink} passHref>
+                Blog
+              </Link>
+            </li>
+            <li className={styles.navListItem}>
+              <Link href="/contact" className={styles.navLink} passHref>
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </main>
   );
